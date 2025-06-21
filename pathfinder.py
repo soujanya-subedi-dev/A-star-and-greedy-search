2efdcbef9f45a256e1017810969d0ebde5950050
soujanya-subedi-dev <idebussoujan@gmail.com>
2025-06-21 21:37:19 +0545

    updated

--- pathfinder.py
+++ pathfinder.py
@@ -89,21 +89,32 @@ midpoint_lat = (coordinates[start][0] + coordinates[goal][0]) / 2
 midpoint_lon = (coordinates[start][1] + coordinates[goal][1]) / 2
 m = folium.Map(location=[midpoint_lat, midpoint_lon], zoom_start=8)
 
-# Plot A* path
+# Plot A* path in blue
 for i in range(len(a_star_path) - 1):
     loc1 = coordinates[a_star_path[i]]
     loc2 = coordinates[a_star_path[i + 1]]
     folium.PolyLine([loc1, loc2], color="blue", weight=3, opacity=0.7).add_to(m)
 
-# Add markers
+# Add markers for A* path
 for loc in a_star_path:
     lat, lon = coordinates[loc]
-    folium.Marker([lat, lon], popup=loc).add_to(m)
+    folium.Marker([lat, lon], popup=f"A*: {loc}", icon=folium.Icon(color='blue')).add_to(m)
+
+# Plot Greedy Best-First Search path in red
+for i in range(len(greedy_path) - 1):
+    loc1 = coordinates[greedy_path[i]]
+    loc2 = coordinates[greedy_path[i + 1]]
+    folium.PolyLine([loc1, loc2], color="red", weight=3, opacity=0.7, dash_array='5').add_to(m)
+
+# Add markers for Greedy path
+for loc in greedy_path:
+    lat, lon = coordinates[loc]
+    folium.Marker([lat, lon], popup=f"Greedy: {loc}", icon=folium.Icon(color='red')).add_to(m)
 
 # Save HTML map
 os.makedirs("output", exist_ok=True)
-m.save("output/a_star_path.html")
-print("\nMap saved to output/a_star_path.html")
+m.save("output/a_star_greedy_paths.html")
+print("\nMap saved to output/a_star_greedy_paths.html")
 
 # Save graph structure as JSON
 with open("output/graph.json", "w") as f:
